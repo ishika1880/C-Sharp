eaa99768e905b1e9475d41a43925c2748efe52aa
ishika1880 <ishikachauhan055@gmail.com>
2025-02-26 21:02:57 +0530

    Save local changes before merging

--- Program.cs
+++ Program.cs
@@ -10,23 +10,37 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
-            // Console.WriteLine("enter a no.");
-            // int n = Convert.ToInt32(Console.ReadLine());
-            // Palindrom.palindrom(n);
-            // Amstrong.amstrong(n);
+            Console.WriteLine("enter a no.");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Palindrom.palindrom(n);
+            Amstrong.amstrong(n);
 
-            // Console.WriteLine("Enter a number to calculate the factorial!!");
-            // int num = Convert.ToInt32(Console.ReadLine());
-            // Console.WriteLine($"Factorial of {num} is {factorial(num)}");
+            Console.WriteLine("Enter a number to calculate the factorial!!");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Factorial of {num} is {factorial(num)}");
             
-            // Console.WriteLine("\t \t \t Let's Guess");
-            // Console.WriteLine("Press Enter to Continue...");
-            // Console.ReadLine();
-            // GuessingGame();
+            Console.WriteLine("\t \t \t Let's Guess");
+            Console.WriteLine("Press Enter to Continue...");
+            Console.ReadLine();
+            GuessingGame();
 
-
+            Console.WriteLine("\nRunning Inheritance Example:");
             Run.Execute();
+
+            Console.WriteLine("\nRunning Polymorphism Example:");
             Run2.run2();
+
+            Console.WriteLine("\nRunning Abstraction Example:");
+            RunAbstraction.Run();
+
+            Console.WriteLine("\nRunning Interface Example:");
+            RunInterface.Run();
+
+            Console.WriteLine("\nRunning Order Status Enum Example:");
+            RunEnum1.Run();
+
+            Console.WriteLine("\nRunning Weekdays Enum Example:");
+            RunEnum2.Run();
         }
 
     }
--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4a68674a56738141ae6e976f49055ab3791e018")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f63bb343b16d41ec78aad3c47b74935530d878e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
